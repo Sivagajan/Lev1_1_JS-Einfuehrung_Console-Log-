4135a5602ae6bf8aae78602db363d563ed67dadf
Sivagajan <sivagajan@gmail.com>
2022-07-14 13:22:50 +0200

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+console.log(document.URL)
+console.log('Anton')
+
+let firstName = 'Anton'
+console.log(firstName)
+
+let age = 28
+let job = 'Trainer'
+married = Boolean(true)
+
+console.log(married)
+
+console.log(firstName+' ist ' + age + ' alt und von Beruf ' + job + ' und ist Verheiratet: ' + married )
+
+
